2222bc95909ae129d687e37769a5e526de54eb03
MartijnDB <47593731+MartijnDB@users.noreply.github.com>
2022-07-06 11:51:37 +0200

    Match new version of kreait/firebase-php

--- src/Broadcasters/FSDB.php
+++ src/Broadcasters/FSDB.php
@@ -3,14 +3,13 @@
 namespace ctf0\Firebase\Broadcasters;
 
 use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
+use Kreait\Firebase\Factory;
 use Kreait\Firebase\ServiceAccount;
-use Morrislaptop\Firestore\Factory;
 use Kreait\Firebase\Exception\ApiException;
 use Illuminate\Broadcasting\BroadcastException;
 use Illuminate\Broadcasting\Broadcasters\Broadcaster;
 
-class FSDB extends Broadcaster
+class RTDB extends Broadcaster
 {
     use Common;
 
@@ -19,14 +18,18 @@ class FSDB extends Broadcaster
 
     /**
      * Create a new broadcaster instance.
+     *
+     * @param mixed $config
      */
     public function __construct($config)
     {
-        $sr_account   = ServiceAccount::fromJsonFile(base_path($config['creds_file']));
         $this->config = $config;
-        $this->db     = (new Factory())
-                        ->withServiceAccount($sr_account)
-                        ->createFirestore();
+        $factory    = (new \Kreait\Firebase\Factory())
+            ->withServiceAccount(base_path($config['creds_file']))
+            ->withDatabaseUri($config['databaseURL']);
+
+
+        $this->db = $factory->createDatabase();
     }
 
     /**
@@ -34,20 +37,18 @@ class FSDB extends Broadcaster
      */
     public function broadcast(array $channels, $event, array $payload = [])
     {
-        $db     = $this->db;
         $socket = Arr::pull($payload, 'socket');
 
         foreach ($this->formatChannels($channels) as $channel) {
             try {
-                $coll = $db->collection($this->config['collection_name']);
-                $doc  = $coll->document(md5(Str::uuid()));
-                $doc->set([
-                    'channel'   => $channel,
-                    'data'      => $payload,
-                    'event'     => $event,
-                    'socket'    => $socket,
-                    'timestamp' => round(now()->valueOf()), // return date == to js Date.now()
-                ]);
+                $this->db->getReference($this->config['collection_name'])
+                    ->push([
+                        'channel'   => $channel,
+                        'data'      => $payload,
+                        'event'     => $event,
+                        'socket'    => $socket,
+                        'timestamp' => round(now()->valueOf()), // return date == to js Date.now()
+                    ]);
             } catch (ApiException $e) {
                 throw new BroadcastException($e);
             }
